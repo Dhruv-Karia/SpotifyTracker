47d5756eaf952a98781caa7b307c7eb4aacc761c
maxmiles <maxmiles@uw.edu>
2020-12-24 12:04:42 -0800

    fixed changes

--- app_server.R
+++ app_server.R
@@ -6,16 +6,15 @@ server <- function(input, output) {
     varX <- eval(parse(text=paste0("tracks$", tolower(input$selectX))))
     varY <- eval(parse(text=paste0("tracks$", tolower(input$selectY))))
     plot <- scatterD3(
-      x = varX, y = varY
-      # , point_opacity = 0.5, col_var = tracks$Explicit,
-      # xlab = str_to_title(input$selectX), ylab = str_to_title(input$selectY),
-      # axes_font_size = "160%", col_lab = "Explicit",
-      # tooltip_text = paste0("<strong>Track: </strong>",
-      #                       str_to_title(tracks$Track), "<br>",
-      #                       "<strong>", input$selectX,
-      #                       ": </strong>", varX, "<br>",
-      #                       "<strong>", input$selectY,
-      #                       ": </strong>", varY)
+      x = varX, y = varY, point_opacity = 0.5, col_var = tracks$Explicit,
+      xlab = str_to_title(input$selectX), ylab = str_to_title(input$selectY),
+      axes_font_size = "160%", col_lab = "Explicit",
+      tooltip_text = paste0("<strong>Track: </strong>",
+                           str_to_title(tracks$Track), "<br>",
+                           "<strong>", input$selectX,
+                           ": </strong>", varX, "<br>",
+                           "<strong>", input$selectY,
+                           ": </strong>", varY)
     )
     return(plot)
   })
